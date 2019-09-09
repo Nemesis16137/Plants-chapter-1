50931755b0c755778232ebfef673326b4d540ea2
Nemesis16137 <jharrison2@edisonohio.edu>
2019-09-09 18:11:53 -0400

    1 file modified

--- index.js
+++ index.js
@@ -5,13 +5,3 @@ import './tinleyhand.css';
 
 
 //write Javascript code!
-function changingImg(){
-    document.getElementById("y").src="./images/blanket.jpg"
-    document.getElementById("1").src="./images/flower.jpg"
-    document.getElementById("y").src="./images/rugosa.jpg"
-    document.getElementById("2").src="./images/txlogo.jpg"
-    document.getElementById("y").src="./images/txlogosm.jpg"
-    document.getElementById("y").src="./images/bluestem.jpg"
- 
-
-}
