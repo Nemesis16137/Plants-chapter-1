92a7f1aaf5efa2d895cfb92813ab1f34005dc018
Nemesis16137 <jharrison2@edisonohio.edu>
2019-09-09 17:07:30 -0400

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 //Import stylesheets
 import './style.css';
 import './tinley.css';
-import './tinleyhand.css';
+import './tinleyhand.css'; 
+
 
 //write Javascript code!
+function changingImg(){
+    document.getElementById("y").src="./images/blanket.jpg"
+    document.getElementById("y").src="./images/flower.jpg"
+    document.getElementById("y").src="./images/rugosa.jpg"
+    document.getElementById("y").src="./images/txlogo.jpg"
+    document.getElementById("y").src="./images/txlogosm.jpg"
+    document.getElementById("y").src="./images/bluestem.jpg"
+ 
+
+}
